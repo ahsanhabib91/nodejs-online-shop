c352184a79397da471c4c185fc029319db8fe276
ahsanhabib91 <habibibnakther@gmail.com>
2019-05-13 19:51:02 +0600

    Edit: 404 controller updated

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express'); // returns a function reference
-const bodyParser = require('body-parser');
+const express = require("express"); // returns a function reference
+const bodyParser = require("body-parser");
 
 const app = express(); // app is a valid request handler function
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', adminData.routes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    return res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 // console.log(__dirname);
